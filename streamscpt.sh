eafd45cb28095ebaf5c943f04b8704a34ad5aae3
Tanmay Bhatt <73189869+Tbhatt-Github@users.noreply.github.com>
2022-01-04 20:50:51 +0530

    Added bash script for streaming

---
+++ streamscpt.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+raspivid -o - -t 0 -hf -fps 30 -b 6000000 | ffmpeg -re -ar 44100 -ac 2 -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv rtmp://a.rtmp.youtube.com/live2/u7qt-j2qg-rfzy-1g4y-1ftx
